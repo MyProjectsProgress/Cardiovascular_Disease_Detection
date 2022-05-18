f7472595b163abd4e4f5b68c522f26f07941bc15
Ahmed raouf <araouf481@hotmail.com>
2022-05-18 20:15:37 +0200

    edit sim frint end

--- Frontend/client.js
+++ Frontend/client.js
@@ -1,34 +1,33 @@
-const submitForm = document.getElementById('submit-form')
-const baseEndpoint = "http://localhost:8000/api"
+const submitForm = document.getElementById("submit-form");
+const baseEndpoint = "http://localhost:8000";
 if (submitForm) {
-    submitForm.addEventListener('submit', handleSubmit)
+  submitForm.addEventListener("submit", handleSubmit);
 }
 function handleSubmit(event) {
-    console.log(event)
-    event.preventDefault()
-    const submitEndpoint = `$(baseEndpoint)/token/`
-    let submitFormData = new FormData(submitForm)
-    let submitObjectData = Object.fromEntries (submitFormData)
-    let bodyStr = JSON.stringify(submitObjectData)
-    console.log(submitObjectData, bodyStr)
+  //   console.log(event);
+  event.preventDefault();
+  const submitEndpoint = `${baseEndpoint}/add/`;
+  let submitFormData = new FormData(submitForm);
+  let submitObjectData = Object.fromEntries(submitFormData);
+  let bodyStr = JSON.stringify(submitObjectData);
+  console.log(submitObjectData, bodyStr);
 
-    const options = {
-        method: "post",
-        headers: {
-            "ContentType": "application/json"
-        },
-        body: bodyStr
-    }
-    fetch(submitEndpoint, options)
-    .then (response=>{
-        console.log(response)
-        return response.json()
-    } )
-    .then(x => {
-        console.log(x)
-    }) 
-    .catch (err=> {
-        console.log ('err',err)
+  const options = {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: bodyStr,
+  };
+  fetch(submitEndpoint, options)
+    .then((response) => {
+      console.log(response);
+      return response.json();
     })
-    
+    .then((x) => {
+      console.log(x);
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
 }
